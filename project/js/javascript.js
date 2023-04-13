36d452428898986d7950afa35ffd14c0f6ca908a
joshuabullough <A02086367@aggies.usu.edu>
2023-04-13 00:23:12 -0600

    javascript challenge complete

--- project/js/javascript.js
+++ project/js/javascript.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     (function init() {
         const myTextArea = document.getElementById('userInput');
@@ -21,22 +20,36 @@ $(document).ready(function () {
 
         editor.setSize("100%", "100%");
 
-        editor.on("change", function(cm) {
+        editor.on("change", function (cm) {
             const code = cm.getValue();
             fillDisplay(code);
         });
     }());
 });
 
-function showHint(){
+function showHint() {
     const hintElem = document.getElementsByClassName('hint')[0];
-    if(hintElem.style.display == 'block'){
+    if (hintElem.style.display == 'block') {
         hintElem.style.display = 'none';
     } else {
         hintElem.style.display = 'block';
-    }  
+    }
 }
 
-function fillDisplay(input){
-    document.getElementById('output').innerHTML = input;
+//currently this can access the whole document. security issue. I need to use Iframe
+function fillDisplay(input) {
+    try {
+        document.getElementById('inputScripts').innerHTML;
+        let s = document.createElement("script");
+        s.type = "text/javascript";
+        //insert script as text and convert with eval into a usable javascript function.
+        s.append(`try { new Function(eval("\`{ ${input.replace(/\n/g,' ')} }\`"))(); } catch(e) {}`)
+        //insert script element into div
+        document.getElementById('inputScripts').appendChild(s);
+    } catch (e) {
+        //not a good javascript statement
+    } finally {
+        //delete all scripts from the document.
+        document.getElementById('inputScripts').innerHTML = '';
+    }
 }
